1b9c6776d67b891c92dca6262f723a43ca0da626
faust182 <faust182@gmail.com>
2021-05-12 22:55:57 +0300

    Revert "Revert "GetStructFromInputCsvFile split into two parts""
    This reverts commit 23314c40241a788328a8cf7b19bbd7f2d167543a.

--- ParseCSV/ParseCSV/Program.cs
+++ ParseCSV/ParseCSV/Program.cs
@@ -11,9 +11,8 @@ namespace ParseCSV
 
             var input = Helper.GetInputData();
             //d:\Anton\Work\C#\ParseCSV\ParseCSV\Example meters.CSV
-            // it add for test git!!!
-            var newMonth = new Integrator(input.path_in, input.month, input.year);
-            if (!string.IsNullOrEmpty(input.path_out)) newMonth.pathForOutFile = input.path_out;
+            var newMonth = new Integrator(input.PathInputFile, input.Month, input.Year);
+            if (!string.IsNullOrEmpty(input.PathOutputFile)) newMonth.pathForOutFile = input.PathOutputFile;
             newMonth.CreateOutputFile();
         }
     }
